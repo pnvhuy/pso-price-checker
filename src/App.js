f00e4cfa13191558e9e3a843b7e345768a6afc7f
Phaolo Nguyen <pnvhuy@gmail.com>
2020-05-28 19:45:30 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Form from './components/form/Form';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+		  
+        <div className="main-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to PSO Price Checker</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div>
+			<Form />
+        </div>
       </div>
     );
   }
---
+++ src/components/form/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+  	render() {
+		return (
+			<div className="cm-form">
+				<div>
+					<p>This is my form</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Form;
